6e8d678e61c342d947d7679c829e5bf570ac41a1
Gag Pertosyan <gagik.petrosyan@sideways6.com>
2022-09-29 18:15:31 +0400

    initial commit

---
+++ src/components/mainLayouts/header/Header.tsx
@@ -0,0 +1,101 @@
+import { NavLink } from 'react-router-dom';
+import { updateLanguageToEnglish, updateLanguageToRussian } from '../../../translations/i18n';
+import styles from './style.module.scss'
+import i18n from "i18next";
+import { useState } from 'react';
+import { useTranslation } from "react-i18next";
+
+function Header() {
+
+    const [language, setLanguage] = useState(i18n.language);
+
+    const { t } = useTranslation()
+
+    return (
+        <header className={styles['app-header']}>
+            <div className="container-big">
+                <div className={styles['app-header-mainpanel']}>
+                    <div className={styles['app-brand']}>
+                        <NavLink to="home">
+                            LOGO
+                        </NavLink>
+                    </div>
+                    <nav className={styles['app-navigation']}>
+                        <ul>
+                            <li>
+                                <NavLink
+                                    to="projects"
+                                    className={({ isActive }) =>
+                                        isActive ? styles['active'] : ""
+                                    }
+                                >
+                                    {t('projects')}
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="aboutUs"
+                                    className={({ isActive }) =>
+                                        isActive ? styles['active'] : ""
+                                    }
+                                >
+                                    {t('aboutCompany')}
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="contacts"
+                                    className={({ isActive }) =>
+                                        isActive ? styles['active'] : ""
+                                    }
+                                >
+                                    {t('contacts')}
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className={styles['user-actions']}>
+                        <div className={styles['language-switcher']}>
+                            <ul>
+                                <li
+                                    className={language === 'ru' ? styles['active'] : ""}
+                                    onClick={() => {
+                                        updateLanguageToRussian();
+                                        setLanguage(i18n.language);
+                                    }}
+                                >
+                                    RU
+                                </li>
+                                <li
+                                    className={language === 'en' ? styles['active'] : ""}
+                                    onClick={() => {
+                                        updateLanguageToEnglish();
+                                        setLanguage(i18n.language);
+                                    }}
+                                >
+                                    EN
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={styles['header-button']}>
+                            <div className={styles['button-overlay']}>
+                                <NavLink to="newProject">
+                                    {t('startProject')}
+                                </NavLink>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles['mobile-button']}>
+                        <button type='button'>
+                            <span className={styles['circle']}></span>
+                            <span className={styles['circle']}></span>
+                            <span className={styles['circle']}></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/pages/NotFoundPage404.tsx
@@ -0,0 +1,5 @@
+export const NotFoundPage404 = () => {
+  return (
+    <h1>Page Not Found</h1>
+  )
+}
